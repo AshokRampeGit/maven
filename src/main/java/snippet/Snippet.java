46b0e406b7251ff5d11e55279f012861f6e79bee
r.ashok27@outlook.com <r.ashok27@outlook.com>
2021-05-15 13:10:24 +0530

    18

--- src/main/java/snippet/Snippet.java
+++ src/main/java/snippet/Snippet.java
@@ -4,5 +4,9 @@ public class Snippet {
 	public static void main(String[] args) {
 		System.out.println("opened success");
 	}
+
+	public static void main(String[] args) {
+		System.out.println("opened success dee");
+	}
 }
 
